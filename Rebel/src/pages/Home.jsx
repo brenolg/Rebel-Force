fde1ea8e38e6dfa7fbd417ce4bc6cd59e240de96
brenolg <brenolg@yahoo.com.br>
2023-05-12 22:25:09 -0300

    feat: inciando a pagina home

--- Rebel/src/pages/Home.jsx
+++ Rebel/src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Home.css';
+import xWing from '../images/home-x-wing.png';
+import TIE from '../images/home-tie.png';
 
 export default function Home() {
   return (
 
-    <main>
-      <div>Home</div>
+    <main className="main-home">
+
+      <div className="home-title-container">
+        <img className="home-x-wing" src={xWing} alt="x-wing" />
+        <img className="home-TIE" src={TIE} alt="TIE-fighter" />
+        <h1 className="home-title">Rebel Force</h1>
+      </div>
+
+      <div className="home-nav">
+        <button className="home-button" type="button">Play</button>
+        <button className="home-button" type="button">Rules</button>
+      </div>
     </main>
 
   );
